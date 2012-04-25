95db896af21d7be1394c7bd578993498b188aee2
Ahmed Salem <ahmed.elsayed.salem@gmail.com>
2012-04-25 18:06:30 +0200

    closes #3

--- controllers/tatelecom.php
+++ controllers/tatelecom.php
@@ -1,4 +1,7 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
+
+defined('SYSPATH') or die('No direct script access.');
+
 /**
  * Clickatell HTTP Post Controller
  *
@@ -12,168 +15,191 @@
  * @copyright  Ushahidi - http://www.ushahidi.com
  * @license	   http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
  */
-
 class Tatelecom_Controller extends Controller {
 
-	private $request = array();
+    private $request = array();
 
-	public function __construct() {
-		$this -> request = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST : $_GET;
-	}
+    public function __construct() {
+        $this->request = ($_SERVER['REQUEST_METHOD'] == 'POST') ? $_POST : $_GET;
+    }
 
-	/**
-	 * tatelecom activation method
-	 * to test: http://localhost/tatelecom/activate/0100123456/123456
-	 * @param msisdn 11 number
-	 * @param code 8 numbers
-	 * @return
-	 */
+    /**
+     * tatelecom activation method
+     * to test: http://localhost/tatelecom/activate/0100123456/123456
+     * @param msisdn 11 number
+     * @param code 8 numbers
+     * @return
+     */
+    function activate($msisdn, $code) {
+        Kohana::log('debug', "welcome to Ta-Telecome, Activation , msisdn [ " . $msisdn . " ] Code is [ " . $code . " ]");
+        //put all inputs in array to validate them
+        $inputs = array();
 
-	function activate($msisdn, $code) {
-		
-		// Define error codes for this view.
-		define("ER_CODE_VERIFIED", 0);
-		define("ER_CODE_NOT_FOUND", 1);
-		define("ER_CODE_ALREADY_VERIFIED", 3);
-		
-		$missing_info = TRUE;
-		$filter = " ";
-		$errno = ER_CODE_ALREADY_VERIFIED;
-		
-		if (isset($msisdn) AND ! empty($msisdn) AND isset($code) AND ! empty($code)){
-			$filter = "alert.alert_type=1 AND alert_code='" . strtoupper($code) . "' AND alert_recipient='" . $msisdn . "' ";
-			$missing_info = FALSE;
-		}
+        // Define error codes for this view.
+        define("ER_CODE_VERIFIED", 0);
+        define("ER_CODE_NOT_FOUND", 1);
+        define("ER_CODE_ALREADY_VERIFIED", 3);
 
-		
-	//validate code and msisdn
-            $validate = Validation::factory($this -> request)
-			->pre_filter('trim')
-			->add_rules('msisdn', 'numeric', 'length[11]')
-			->add_rules('code','numeric','length[4]');
-		
 
-		if (!$missing_info) {
-			$alert_check = ORM::factory('alert') -> where($filter) -> find();
+        $missing_info = TRUE;
+        $filter = " ";
+        $errno = ER_CODE_ALREADY_VERIFIED;
 
-			// IF there was no result
-			if (!$alert_check -> loaded) {
-				$errno = ER_CODE_NOT_FOUND;
-			} elseif ($alert_check -> alert_confirmed) {
-				$errno = ER_CODE_ALREADY_VERIFIED;
-			} else {
-				// SET the alert as confirmed, and save it
-				$alert_check -> set('alert_confirmed', 1) -> save();
-				$errno = ER_CODE_VERIFIED;
-			}
-		} else {
-			$errno = ER_CODE_NOT_FOUND;
-		}
-		
-		//log everything
-		Kohana::log('debug', 'MSISDN: ' . $msisdn . ' code: ' . $code . ' => ' . $errno);
-		
-		die(''.$errno);
+        if (isset($msisdn) AND !empty($msisdn) AND isset($code) AND !empty($code)) {
+            $filter = "alert.alert_type=1 AND alert_code='" . strtoupper($code) . "' AND alert_recipient='" . $msisdn . "' ";
+            $missing_info = FALSE;
+        }
 
-	}
 
-	function index($key = NULL) {
-		if (isset($this -> request['from'])) {
-			$message_from = $this -> request['from'];
-			// Remove non-numeric characters from string
-			$message_from = preg_replace("#[^0-9]#", "", $message_from);
-		}
+        /*
+         * MSISDN will look like 20xxxxxxxxx (12 numbers)
+         * code consist of 4 digits
+         * remove all spaces before, after and inside the code
+         * remove any characters inside the code
+         * return 0 for success and 1 for anything
+         */
 
-		if (isset($this -> request['to'])) {
-			$message_to = $this -> request['to'];
-			// Remove non-numeric characters from string
-			$message_to = preg_replace("#[^0-9]#", "", $message_to);
-		}
+        //remove non nummeric from code var , if we have 1a2b3c4 result would be 1234
+        $new_string = preg_replace("/[^0-9]/", "", $code);
+        //echo $new_string;
+        $n_code = $new_string;
 
-		if (isset($this -> request['text'])) {
-			$message_description = $this -> request['text'];
-		}
+        $inputs[] = $msisdn;
+        $inputs[] = $n_code;
 
-		if (!empty($message_from) AND !empty($message_description)) {
-			// Is this a valid Clickatell Key?
-			$keycheck = ORM::factory('clickatell') -> where('clickatell_key', $key) -> find(1);
+        //print final values in array
+        //print_r($inputs);
+        $validate = new Validation($inputs);
+        $validate->pre_filter('trim');
+        $validate->add_rules('msisdn', 'numeric', 'length[12]');
+        $validate->add_rules('code', 'numeric', 'length[4]');
+        // Test to see if things passed the rule checks
+        if ($validate->validate()) {
+            // Yes! everything is valid , we need to send 0
+            Kohana::log('debug', 'Submission validated and submitted correctly');
 
-			if ($keycheck -> loaded == TRUE) {
-				sms::add($message_from, $message_description, $message_to);
+            $errno = ER_CODE_VERIFIED;
+        }
+        // No! We have validation errors, we need to send 1 for error
+        else {
+            $errno = ER_CODE_NOT_FOUND;
+            // populate the error fields, if any
+            Kohana::log('debug', 'Submission have some error , inputs was msisdn[' . $msisdn . '], code [' . $code . ']');
+        }
 
-			}
-		}
-	}
-	
-	
-	public function geocode(){
-		print_r( Tatelecom_Controller::reverse_geocode(30.039043,31.408539));
-		//30.040311,31.398298
-	}
-	
-	/**
-	 * Reverse Geocode a point
-	 *
-	 * @author
-	 * @param   double  $latitude
-	 * @param   double  $longitude
-	 * @return  string  closest approximation of the point as a display name
-	 */
-	public static function reverse_geocode($latitude,$longitude)
-	{
-		if ($latitude AND $longitude)
-		{
-			$url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='.$latitude.','.$longitude.'&sensor=false&language=ar';
-			$ch = curl_init();
-			curl_setopt($ch,CURLOPT_URL,$url);
-			curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-			curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
-			$json = curl_exec($ch);
-			curl_close($ch);
+        if ($missing_info) {
+            $alert_check = ORM::factory('alert')->where($filter)->find();
 
-			$location = json_decode($json, false);
-			print_r($location->results);
+            // IF there was no result
+            if (!$alert_check->loaded) {
+                $errno = ER_CODE_NOT_FOUND;
+            } elseif ($alert_check->alert_confirmed) {
+                $errno = ER_CODE_ALREADY_VERIFIED;
+            } else {
+                // SET the alert as confirmed, and save it
+                $alert_check->set('alert_confirmed', 1)->save();
+                $errno = ER_CODE_VERIFIED;
+            }
+        } else {
+            $errno = ER_CODE_NOT_FOUND;
+        }
 
-			return $location->results[0]->formatted_address;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	
-	
-	public function test(){
-		echo sms::send('201001374126', 'zabatak', 'test') .'';
-		//sms::send($alertee->alert_recipient, $sms_from, $sms_message)
-	}
-	
-	/**
-    * Check for CURL PHP module
-    * @access private
-    */
+        //log everything
+        Kohana::log('debug', 'MSISDN: ' . $msisdn . ' code: ' . $code . ' => ' . $errno);
+        //print $errno;
+        die('' . $errno);
+    }
+
+    function index($key = NULL) {
+        if (isset($this->request['from'])) {
+            $message_from = $this->request['from'];
+            // Remove non-numeric characters from string
+            $message_from = preg_replace("#[^0-9]#", "", $message_from);
+        }
+
+        if (isset($this->request['to'])) {
+            $message_to = $this->request['to'];
+            // Remove non-numeric characters from string
+            $message_to = preg_replace("#[^0-9]#", "", $message_to);
+        }
+
+        if (isset($this->request['text'])) {
+            $message_description = $this->request['text'];
+        }
+
+        if (!empty($message_from) AND !empty($message_description)) {
+            // Is this a valid Clickatell Key?
+            $keycheck = ORM::factory('clickatell')->where('clickatell_key', $key)->find(1);
+
+            if ($keycheck->loaded == TRUE) {
+                sms::add($message_from, $message_description, $message_to);
+            }
+        }
+    }
+
+    public function geocode() {
+        print_r(Tatelecom_Controller::reverse_geocode(30.039043, 31.408539));
+        //30.040311,31.398298
+    }
+
+    /**
+     * Reverse Geocode a point
+     *
+     * @author
+     * @param   double  $latitude
+     * @param   double  $longitude
+     * @return  string  closest approximation of the point as a display name
+     */
+    public static function reverse_geocode($latitude, $longitude) {
+        if ($latitude AND $longitude) {
+            $url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' . $latitude . ',' . $longitude . '&sensor=false&language=ar';
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            $json = curl_exec($ch);
+            curl_close($ch);
+
+            $location = json_decode($json, false);
+            print_r($location->results);
+
+            return $location->results[0]->formatted_address;
+        } else {
+            return false;
+        }
+    }
+
+    public function test() {
+        echo sms::send('201001374126', 'zabatak', 'test') . '';
+        //sms::send($alertee->alert_recipient, $sms_from, $sms_message)
+    }
+
+    /**
+     * Check for CURL PHP module
+     * @access private
+     */
     function _chk_curl() {
         if (!extension_loaded('curl')) {
             return "This SMS API class can not work without CURL PHP module! Try using fopen sending method.";
         }
     }
-	
-	 /**
-    * CURL sending method
-    * @access private
-    */
+
+    /**
+     * CURL sending method
+     * @access private
+     */
     function _curl($command) {
         $this->_chk_curl();
-        $ch = curl_init ($command);
-        curl_setopt ($ch, CURLOPT_HEADER, 0);
-        curl_setopt ($ch, CURLOPT_RETURNTRANSFER,1);
-        curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER,0);
+        $ch = curl_init($command);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         if ($this->curl_use_proxy) {
-            curl_setopt ($ch, CURLOPT_PROXY, $this->curl_proxy);
-            curl_setopt ($ch, CURLOPT_PROXYUSERPWD, $this->curl_proxyuserpwd);
+            curl_setopt($ch, CURLOPT_PROXY, $this->curl_proxy);
+            curl_setopt($ch, CURLOPT_PROXYUSERPWD, $this->curl_proxyuserpwd);
         }
-        $result=curl_exec ($ch);
-        curl_close ($ch);
+        $result = curl_exec($ch);
+        curl_close($ch);
         return $result;
     }
 
